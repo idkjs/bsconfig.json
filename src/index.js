e2559d507c81d5ff8012e85527df787de99c9d03
Alain Armand <aarmand.inbox@gmail.com>
2020-11-18 13:21:58 +0100

    inish

---
+++ src/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const inquirer = require("inquirer");
+const path = require('path');
+const { writeFile, readdir, readFile } = require("fs").promises;
+
+const configFiles = {};
+const configFolderPath = path.resolve(__dirname, 'config');
+
+
+(async () => {
+
+  const files = await readdir(configFolderPath).catch(console.log);
+
+  for (let i of files) {
+    // framework name is situated between 2 dots eg- react between 2 '.'(s)
+    const frameworkName = i.split('.')[1];
+    configFiles[frameworkName] = path.join(configFolderPath, i);
+  }
+
+  const { framework } = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Pick the framework you're using:",
+      name: "framework",
+      choices: Object.keys(configFiles),
+    }
+  ]);
+
+  const config = await readFile(configFiles[framework]).catch(console.log);
+  const bsconfig = path.join(process.cwd(), 'bsconfig.json');
+
+
+  await writeFile(bsconfig, config, 'utf8');
+
+  console.log("bsconfig.json successfully created");
+})()
